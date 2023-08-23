de4700f1404144bc2fb77a46d58e28d2fb43d110
Jaime Pineda <jpin730@pm.me>
2023-08-23 17:19:43 -0600

    feat: :sparkles: add get country service

--- bin/main.dart
+++ bin/main.dart
@@ -2,4 +2,6 @@ import 'package:dart_fundamentals_packages/my_package.dart' as my_package;
 
 void main(List<String> arguments) {
   my_package.getReqResUsersService();
+
+  my_package.getCountryService();
 }
--- lib/my_package.dart
+++ lib/my_package.dart
@@ -1,3 +1,4 @@
+import 'package:dart_fundamentals_packages/classes/country.dart';
 import 'package:http/http.dart' as http;
 
 import 'package:dart_fundamentals_packages/classes/reqres_response.dart';
@@ -9,5 +10,20 @@ void getReqResUsersService() {
     print('page: ${resReqResponse.page}');
     print('per_page: ${resReqResponse.perPage}');
     print('id of third element: ${resReqResponse.data[2].id}');
+  });
+}
+
+void getCountryService() {
+  final url = Uri.parse('https://restcountries.com/v3.1/alpha/us');
+  http.get(url).then((res) {
+    final country = countryFromJson(res.body).first;
+    print('Country: ${country.name.official}');
+    print('Population: ${country.population}');
+    print('Borders: ${country.borders.join(", ")}');
+    print('Language: ${country.languages.eng}');
+    print('Lat: ${country.latlng[0]}');
+    print('Lng: ${country.latlng[1]}');
+    print('Currency: ${country.currencies}');
+    print('Flag: ${country.flag}');
   });
 }
